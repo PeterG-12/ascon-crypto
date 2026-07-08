c3aa06c5264de8383223a3ac8420f8164e48a53e
PeterG-12 <galpeter05@gmail.com>
2026-07-08 12:53:56 +0300

    Initial commit

---
+++ AsconCrypto.srcs/sources_1/new/ascon_core.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+library work;
+use work.ascon_package.ALL;
+
+
+entity ascon_core is
+--  Port ( );
+end ascon_core;
+
+architecture Behavioral of ascon_core is
+
+begin
+    
+
+end Behavioral;
---
+++ AsconCrypto.srcs/sources_1/new/ascon_package.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package ascon_package is
+    type table5_type is array (15 downto 0) of std_logic_vector(63 downto 0); -- Type for Table 5. The constants const_i to derive round constants of the Ascon permutations
+
+    constant TABLE5 : table5_type := (
+        x"000000000000003c",
+        x"000000000000002d",
+        x"000000000000001e",
+        x"000000000000000f",
+        x"00000000000000f0",
+        x"00000000000000e1",
+        x"00000000000000d2",
+        x"00000000000000c3",
+        x"00000000000000b4",
+        x"00000000000000a5",
+        x"0000000000000096",
+        x"0000000000000087",
+        x"0000000000000078",
+        x"0000000000000069",
+        x"000000000000005a",
+        x"000000000000004b"
+    );
+    
+
+end ascon_package;
