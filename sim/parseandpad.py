19bdaf777ddffbe85e76240a9762db3670e5e182
PeterG-12 <galpeter05@gmail.com>
2026-07-09 11:56:38 +0300

    Added parsing and padding to testbench

---
+++ sim/parseandpad.py
@@ -0,0 +1,42 @@
+# Script used for debugging parse_and_pad
+debug = True
+
+def parse_and_pad(hexstring : str):
+    M = list((hexstring[0+i:16+i] for i in range(0, len(hexstring), 16)))
+
+    print(M)
+
+    for i in range(0, len(M)):
+        new_str = ""
+        old_str = M[i]
+
+        old_str_length = len(old_str)
+        for j in range(0, old_str_length, 2):
+            new_str += old_str[old_str_length - 2 - j: old_str_length - j]
+        M[i] = new_str
+
+    byte_length = len(hexstring) // 2
+    byte_length %= 8
+    # Get length in bytes of last 64-bit word
+    print(M)
+
+    padded_word = ""
+    if byte_length == 0:
+        padded_word = "0000000000000001"
+        M.append(padded_word)
+    else:
+        last = M[-1] # the last, incomplete word
+        padded_word = last
+        padded_word = "01" + padded_word
+        for i in range(0, 8 - byte_length - 1):
+            padded_word = "00" + padded_word
+        M[-1] = padded_word
+
+    if debug: print(M)    
+    return M
+
+parse_and_pad("00010203040506070809")
+parse_and_pad("0001020304050607")
+parse_and_pad("FF")
+parse_and_pad("FFFFFF")
+parse_and_pad("")
--- sim/test_ascon_hash.py
+++ sim/test_ascon_hash.py
@@ -1,3 +1,5 @@
+from re import M
+import string
 import cocotb
 from cocotb.triggers import Timer, Edge, with_timeout
 from random import randint
@@ -14,9 +16,41 @@ def split(hexstring):
     out_str = hexstring[0:16] + "  " + hexstring[16: 32] + "  " + hexstring[32: 48] + "  " + hexstring[48 : 64] + "  " + hexstring[64:80]
     return out_str
 
-async def generate_clock(dut : copra_stubs.AsconHash256):
+def parse_and_pad(hexstring : str):
+    M = list((hexstring[0+i:16+i] for i in range(0, len(hexstring), 16)))
 
-    count = 0
+    print(M)
+
+    for i in range(0, len(M)):
+        new_str = ""
+        old_str = M[i]
+
+        old_str_length = len(old_str)
+        for j in range(0, old_str_length, 2):
+            new_str += old_str[old_str_length - 2 - j: old_str_length - j]
+        M[i] = new_str
+
+    byte_length = len(hexstring) // 2
+    byte_length %= 8
+    # Get length in bytes of last 64-bit word
+    print(M)
+
+    padded_word = ""
+    if byte_length == 0:
+        padded_word = "0000000000000001"
+        M.append(padded_word)
+    else:
+        last = M[-1] # the last, incomplete word
+        padded_word = last
+        padded_word = "01" + padded_word
+        for i in range(0, 8 - byte_length - 1):
+            padded_word = "00" + padded_word
+        M[-1] = padded_word
+    
+    print(M)
+    return M
+
+async def generate_clock(dut : copra_stubs.AsconHash256):
 
     """Generate clock pulses."""
     for _ in range(10000):
@@ -25,15 +59,7 @@ async def generate_clock(dut : copra_stubs.AsconHash256):
         dut.clk_i.value = 1
         await Timer(5, unit="ns")
 
-        if dut.word_processed_o.value == 1:
-            if count == 2:
-                dut.m_i.value = 1
-            else:
-                dut.m_i.value = 0
-            if count == 0:
-                dut.m_i.value = int(0x0000000000000001)
-                dut.word_left.value = 0
-            count -= 1
+        
 
 async def generate_log(dut : copra_stubs.AsconHash256):
     logger = logging.getLogger("my_testbench")
@@ -55,16 +81,38 @@ async def generate_log(dut : copra_stubs.AsconHash256):
         if dut.core_finished.value == 1:
             logger.warning("Core finished with: %s" % split(hex(dut.core_out.value)))
 
+async def generate_input(dut : copra_stubs.AsconHash256, hexstring : str):
+    logger = logging.getLogger("my_testbench")
+    M_list = parse_and_pad(hexstring)
+    count = len(M_list)
+    i = 0
+
+    dut.m_i.value = int(M_list[i], 16)
+    logger.warning("INPUT: " + M_list[i] + "   " + str(hex(int(M_list[i], 16))[2:]))
+    i += 1
+
+    while dut.finished_o.value != 1:
+        await dut.clk_i.rising_edge
+
+        if dut.word_processed_o.value == 1:
+            if i != count:
+                dut.m_i.value = int(M_list[i], 16)
+                logger.warning("INPUT: " + M_list[i] + "   " + str(hex(int(M_list[i], 16))[2:]))
+                i += 1
+            if i == count:
+                dut.word_left.value = 0
+
 
 @cocotb.test()
 async def test_ascon_hash(dut : copra_stubs.AsconHash256):
     logger = logging.getLogger("my_testbench")
     logger.warning("This is an info message")
-    dut.m_i.value = int(0x0706050403020100)
+    #dut.m_i.value = int(0x0706050403020100)
     dut.start_i.value = 1
     dut.word_left.value = 1
 
     cocotb.start_soon(generate_clock(dut))
+    cocotb.start_soon(generate_input(dut, "00010203040506070809"))
     cocotb.start_soon(generate_log(dut))
 
 
